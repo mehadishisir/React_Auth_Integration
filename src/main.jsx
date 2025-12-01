7c55c68e876f9c3a310bba094778b19c4fd66c48
mehadi <mehadishisir@gmail.com>
2025-12-01 18:17:30 +0600

    add react router , tailwind ,daisy

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Roots from "./components/roots/Roots.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Signup from "./pages/signup/Signup.jsx";
+import Login from "./pages/login/Login.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Roots,
+    children: [
+      { index: true, Component: Home },
+      {
+        path: "signup",
+        Component: Signup,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
