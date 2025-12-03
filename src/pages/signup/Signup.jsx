8917de63b3920ec38111e766e66e4a54be2bfa6f
mehadi <mehadishisir@gmail.com>
2025-12-03 23:16:52 +0600

    add signUp form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Signup = () => {
-  return <div>this is signup page</div>;
+  const handleSignup = (e) => {
+    e.preventDefault();
+    // Handle signup logic here
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("Signup details:", name, email, password);
+  };
+  return (
+    <div className="card  bg-base-100 w-full max-w-sm mx-auto shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-3xl mx-auto">SignUp Now</h1>
+        <form onSubmit={handleSignup} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Sign up</button>
+        </form>
+        <p>
+          Already have an account? please{" "}
+          <Link className="text-blue-500 underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
